82bd65908ab467a32e40e56a9f912c588c704efc
Yaroslava Nahorna <yasya74800@gmail.com>
2025-11-01 12:00:00 +0200

    Create connection to database

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+const { configDotenv } = require("dotenv")
 const express = require("express")
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true }))
+
+configDotenv()
+    const { Sequelize } = require('sequelize');
+    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DRIVER
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log("Database connected...")
+    })
+    .catch(err => {
+        console.log("Error: " + err)
+    })
 
 app.get("/", (req,res) => {
     res.json({ message: "Welcome to sequelize app"} )
